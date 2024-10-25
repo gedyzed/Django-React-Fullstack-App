bc6a05108bdf647cfe4f7e01e2ed077584a24410
Geda <gediizeyuu@gmail.com>
2024-10-25 17:08:51 +0300

    add app to wsgi.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-pasahk*w+sz_e^n&=+8*nxf(_g33%ecqt!s2*^ui(xwg(4%dej
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
